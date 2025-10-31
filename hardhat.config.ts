a33ee4f146bccacf07f511b94adfd689b64652b9
shubhamkunwar10 <fonders@metakul.com>
2025-10-31 20:24:45 +0530

    Add verification Code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,40 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatVerify],
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+    },
+    blockscout: {
+      enabled: false,
+    },
+  },
+  chainDescriptors: {
+    80002: {
+      name: "Polygon Amoy",
+      blockExplorers: {
+        etherscan: {
+          name: "PolygonScan Amoy",
+          url: "https://amoy.polygonscan.com",
+          apiUrl: "https://api.etherscan.io/v2/api",
+        },
+      },
+    },
+    137: {
+      name: "Polygon",
+      blockExplorers: {
+        etherscan: {
+          name: "PolygonScan",
+          url: "https://polygonscan.com",
+          apiUrl: "https://api.etherscan.io/v2/api",
+        },
+      },
+    },
+  },
   solidity: {
     profiles: {
       default: {
