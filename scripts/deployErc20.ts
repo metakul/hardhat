e07fd93fcd12401870c416045f8250ee2fb66d83
shubhamkunwar10 <fonders@metakul.com>
2025-10-31 18:43:02 +0530

    Update erc20 depoyment with props

--- scripts/deployErc20.ts
+++ scripts/deployErc20.ts
@@ -1,26 +1,28 @@
 import { network } from "hardhat";
 
 const { ethers } = await network.connect();
-const contractName = "CustomToken";
-const tokenName = "MetaCoin";
-const tokenSymbol = "MTC";
-const initialSupply = ethers.parseEther("1000000"); // 1 million tokens
 
-async function main() {
+export async function deployErc20({
+  contractName,
+  tokenName,
+  tokenSymbol,
+  initialSupply,
+}: {
+  contractName: string;
+  tokenName: string;
+  tokenSymbol: string;
+  initialSupply: bigint;
+}) {
   const [deployer] = await ethers.getSigners();
-  console.log("Deploying contract with:", deployer.address);
+  console.log(`\n🚀 Deploying ERC20 (${tokenName})...`);
+  console.log("Deployer:", deployer.address);
 
   const Token = await ethers.getContractFactory(contractName);
   const token = await Token.deploy(tokenName, tokenSymbol, initialSupply);
   await token.waitForDeployment();
 
-  console.log(
-    `✅ ${tokenName} (${tokenSymbol}) deployed to:`,
-    await token.getAddress()
-  );
-}
+  const tokenAddress = await token.getAddress();
+  console.log(`✅ ${tokenName} (${tokenSymbol}) deployed at: ${tokenAddress}`);
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  return token;
+}
