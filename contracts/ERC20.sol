d93f65df56a1b2aefb9c8dee37579501da52ec5f
shubhamkunwar10 <fonders@metakul.com>
2025-10-31 19:21:58 +0530

    Add ReentrancyGuard to token

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract CustomToken is ERC20, Ownable {
+contract CustomToken is ERC20, Ownable,ReentrancyGuard {
+    event TokensRecovered(address indexed token, uint256 amount);
+
     constructor(
         string memory name_,
         string memory symbol_,
@@ -15,7 +18,6 @@ contract CustomToken is ERC20, Ownable {
 
     /// @notice 🚨 Recover tokens accidentally sent to this contract (except staking token)
     function recoverTokens(address tokenAddress, uint256 amount) external onlyOwner nonReentrant {
-        require(tokenAddress != address(stakingToken), "cannot recover staking token");
         require(amount > 0, "invalid amount");
 
         IERC20(tokenAddress).transfer(owner(), amount);
